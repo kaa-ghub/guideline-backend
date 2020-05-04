208cf67e6788d80cd356f3718af8ac77f48d18b1
kaa <kaa.unused@yandex.ru>
2020-05-04 19:22:52 +0700

    Добавил liquibase для вервионирования БД и прописал скрипты начальной загрузки

--- src/main/java/virushack/changing/guideline/backend/dto/RecommendationResponseDto.java
+++ src/main/java/virushack/changing/guideline/backend/dto/RecommendationResponseDto.java
@@ -1,11 +1,15 @@
 package virushack.changing.guideline.backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import virushack.changing.guideline.backend.model.Guideline;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecommendationResponseDto {
     private String id;
     private List<Guideline> guideline;
--- src/main/java/virushack/changing/guideline/backend/service/RecommendationService.java
+++ src/main/java/virushack/changing/guideline/backend/service/RecommendationService.java
@@ -4,13 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import virushack.changing.guideline.backend.dto.RecommendationRequestDto;
 import virushack.changing.guideline.backend.dto.RecommendationResponseDto;
+import virushack.changing.guideline.backend.model.Guideline;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
 
     public RecommendationResponseDto getRecommendation(RecommendationRequestDto request) {
-        return null;
+        RecommendationResponseDto response =
+                new RecommendationResponseDto(UUID.randomUUID().toString(), List.of(new Guideline()));
+
+        return response;
     }
 
 }
