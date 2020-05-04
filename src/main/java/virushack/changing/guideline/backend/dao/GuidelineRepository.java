b5a007109f3f6a879afc0d18eb516e75a8e2ce6e
kaa <kaa.unused@yandex.ru>
2020-05-04 15:20:05 +0700

    Add cors

--- src/main/java/virushack/changing/guideline/backend/dao/GuidelineRepository.java
+++ src/main/java/virushack/changing/guideline/backend/dao/GuidelineRepository.java
@@ -2,8 +2,10 @@ package virushack.changing.guideline.backend.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import virushack.changing.guideline.backend.model.Guideline;
 
+@CrossOrigin
 @RepositoryRestResource
 public interface GuidelineRepository extends JpaRepository<Guideline, String> {
 }
--- src/main/java/virushack/changing/guideline/backend/dao/PersonRepository.java
+++ src/main/java/virushack/changing/guideline/backend/dao/PersonRepository.java
@@ -1,11 +1,13 @@
 package virushack.changing.guideline.backend.dao;
 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import virushack.changing.guideline.backend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
+@CrossOrigin
 @RepositoryRestResource
 public interface PersonRepository extends JpaRepository<Person, UUID> {
 }
